71c7e54005aed5c7faaeee15b2503e317b596680
seratonin0073 <mizko2000y@gmail.com>
2021-07-23 18:20:37 +0300

    AddProject

---
+++ GuitarApp/StartMenu.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GuitarApp
+{
+    /// <summary>
+    /// Логика взаимодействия для StartMenu.xaml
+    /// </summary>
+    public partial class StartMenu : Window
+    {
+        private User user;
+        public StartMenu()
+        {
+            user = new User("login", "1111");
+            InitializeComponent();
+        }
+
+        private void Enter_Click(object sender, RoutedEventArgs e)
+        {
+            if (user.Login == Login.Text && user.Password == Password.Password)
+            {
+                MessageBox.Show("Пароль введен верно!", "Поздравление!!!", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            else
+            {
+                MessageBox.Show("Пароль введен не верно!", "Предупреждение!", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+        }
+    }
+}
---
+++ GuitarApp/User.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuitarApp
+{
+    class User
+    {
+        private string login;
+        private string password;
+
+        public string Login
+        {
+            get { return login; }
+        }
+
+        public string Password
+        {
+            get { return password; }
+        }
+
+        public User(string login, string password)
+        {
+            this.login = login;
+            this.password = password;
+        }
+    }
+}
